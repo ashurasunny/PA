24ab6f001a621d5942a7caa637198e7258684b61
tracer-ics2015 <tracer@njuics.org>
2015-10-26 21:16:32 +0000

    >  compile NEMU 141220000 wang Linux wang 4.2.3-1-ARCH #1 SMP PREEMPT Sat Oct 3 18:52:50 CEST 2015 x86_64 GNU/Linux  21:16:32 up 34 min,  2 users,  load average: 0.05, 0.07, 0.11 67187110f4966ea5fee39427db66b20a830df1ab

--- nemu/src/cpu/exec/data-mov/push-tempalte.h
+++ nemu/src/cpu/exec/data-mov/push-tempalte.h
@@ -1,1 +1,20 @@
+#include "cpu/exec/template-start.h"
 
+#define instr push
+
+static void do_execute () {
+   if(2 == DATA_BYTE)
+    {
+        cpu.esp-=2;
+        MEM_W(cpu.esp,op_src->val);
+    }
+ else if(4 == DATA_BYTE)
+    {
+    cpu.esp-=4;
+      MEM_W(cpu.esp,op_src->val);
+    }
+}
+
+make_instr_helper(r)
+
+#include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/data-mov/push.h
+++ nemu/src/cpu/exec/data-mov/push.h
@@ -1,6 +1,6 @@
 #ifndef PUSH_H_INCLUDED
 #define PUSH_H_INCLUDED
 
-
+make_helper(push_r_v);
 
 #endif // PUSH_H_INCLUDED
