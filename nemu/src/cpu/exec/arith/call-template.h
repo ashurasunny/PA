3d866cbd805d0276532fcf44719081af12ac57dd
tracer-ics2015 <tracer@njuics.org>
2015-10-26 15:18:21 +0000

    >  compile NEMU 141220000 wang Linux wang 4.2.3-1-ARCH #1 SMP PREEMPT Sat Oct 3 18:52:50 CEST 2015 x86_64 GNU/Linux  15:18:21 up  1:16,  2 users,  load average: 0.48, 0.16, 0.09 88c738a428f89819bb36c7a6e232258a67ee59c6

--- nemu/src/cpu/exec/arith/call-template.h
+++ nemu/src/cpu/exec/arith/call-template.h
@@ -6,13 +6,13 @@
 static void do_execute () {
    if(2 == DATA_BYTE)
     {
-        cpu.esp-=2;printf("%d",cpu.esp);
+        cpu.esp-=2;
         MEM_W(cpu.esp,cpu.eip);
         cpu.eip = (cpu.eip+op_src->val)&0x0000ffff;
     }
  else if(4 == DATA_BYTE)
     {
-        cpu.esp-=4;printf("%d",cpu.esp);
+        cpu.esp-=4;
       MEM_W(cpu.esp,cpu.eip);
         cpu.eip += op_src->val;
     }
