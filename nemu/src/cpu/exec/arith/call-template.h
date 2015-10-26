b65ff9820fd86ecb31d25e694a93523ef2802861
tracer-ics2015 <tracer@njuics.org>
2015-10-26 14:20:52 +0000

    >  compile NEMU 141220000 wang Linux wang 4.2.3-1-ARCH #1 SMP PREEMPT Sat Oct 3 18:52:50 CEST 2015 x86_64 GNU/Linux  14:20:52 up 19 min,  2 users,  load average: 0.11, 0.15, 0.27 56da8d185f27d92ed5e28d7cd70fc40127858367

--- nemu/src/cpu/exec/arith/call-template.h
+++ nemu/src/cpu/exec/arith/call-template.h
@@ -4,27 +4,26 @@
 #define instr call
 
 static void do_execute () {
-    if(2 == DATA_BYTE)
+   if(2 == DATA_BYTE)
     {
-        cpu.esp-=2;
-        MEM_W(cpu.esp,cpu.eip);
-        cpu.eip = (cpu.eip+op_src->val)&0x0000ffff;
+       // cpu.esp-=2;
+    //    MEM_W(cpu.esp,cpu.eip);
+       // cpu.eip = (cpu.eip+op_src->val)&0x0000ffff;
     }
-    if(4 == DATA_BYTE)
+ if(4 == DATA_BYTE)
     {
         cpu.esp-=4;
-        MEM_W(cpu.esp,cpu.eip);
+      MEM_W(cpu.esp,cpu.eip);
         cpu.eip += op_src->val;
     }
-    /**
-    cpu.esp -= 4;
+   /***** cpu.esp -= 4;
     MEM_W(cpu.esp,cpu.eip);
-    #if DATA_BYTE == 1
+    #if DATA_BYTE == 2
          cpu.eip = (cpu.eip + op_src->val)&0x0000ffff;
     #else
          cpu.eip += op_src->val;
     #endif // DATA_BYTE
-	print_asm_template1();**/
+	print_asm_template1();****/
 }
 
 make_instr_helper(i)
