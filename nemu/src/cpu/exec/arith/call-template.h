66bbd100714c258eddcd429b93a1f79a9c52c441
tracer-ics2015 <tracer@njuics.org>
2015-10-26 10:58:06 +0000

    >  compile NEMU 141220000 wang Linux wang 4.2.3-1-ARCH #1 SMP PREEMPT Sat Oct 3 18:52:50 CEST 2015 x86_64 GNU/Linux  10:58:06 up  2:40,  2 users,  load average: 0.04, 0.11, 0.09 84ead7c9db02ee648b721cd7ca0d2bc2be1bad5

---
+++ nemu/src/cpu/exec/arith/call-template.h
@@ -0,0 +1,22 @@
+
+#include "cpu/exec/template-start.h"
+
+#define instr call
+
+static void do_execute () {
+    cpu.esp -= 4;
+    MEM_W(cpu.esp,cpu.eip);
+
+    #if DATA_BYTE == 1
+         cpu.eip = (cpu.eip + op_src->val)&0x0000ffff;
+    #else
+         cpu.eip += op_src->val;
+    #endif // DATA_BYTE
+	print_asm_template1();
+}
+
+make_instr_helper(i)
+make_instr_helper(rm)
+
+#include "cpu/exec/template-end.h"
+
---
+++ nemu/src/cpu/exec/arith/call.h
@@ -0,0 +1,7 @@
+#ifndef CALL_H_INCLUDED
+#define CALL_H_INCLUDED
+make_helper(call_i_v);
+make_helper(call_rm_v);
+
+
+#endif // CALL_H_INCLUDED
