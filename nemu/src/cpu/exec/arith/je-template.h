bfade4d0fd93b9c557268bc125e602f55415eeb1
tracer-ics2015 <tracer@njuics.org>
2015-10-27 09:45:02 +0000

    >  compile NEMU 141220000 wang Linux wang 4.2.3-1-ARCH #1 SMP PREEMPT Sat Oct 3 18:52:50 CEST 2015 x86_64 GNU/Linux  09:45:02 up 53 min,  2 users,  load average: 0.10, 0.09, 0.12 700b1cf3cedcce9fa019815587a8f1f709d3310

---
+++ nemu/src/cpu/exec/arith/je-template.h
@@ -0,0 +1,21 @@
+
+#include "cpu/exec/template-start.h"
+
+#define instr je
+
+static void do_execute () {
+if(cpu.EFLAGS.ZF == 1)
+{
+    #if DATA_BYTE == 2
+         cpu.eip = (cpu.eip + op_src->val)&0x0000ffff;
+    #else
+         cpu.eip += op_src->val;
+    #endif // DATA_BYTE
+}
+	print_asm_template1();
+}
+
+make_instr_helper(i)
+//make_instr_helper(rm)
+
+#include "cpu/exec/template-end.h"
---
+++ nemu/src/cpu/exec/arith/je.h
@@ -0,0 +1,6 @@
+#ifndef JE_H_INCLUDED
+#define JE_H_INCLUDED
+
+make_helper(je_i_b);
+make_helper(je_i_v);
+#endif // JE_H_INCLUDED
